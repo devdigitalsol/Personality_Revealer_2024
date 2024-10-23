fda180a8b0e0871dce2f192a9ab0fc39aa1a20e1
princey818 <princey818@gmail.com>
2024-10-23 11:42:13 +0530

    description page done

--- src/pages/Description.jsx
+++ src/pages/Description.jsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 import { useNavigate } from "react-router-dom";
+import LOGO2 from "./../assets/logo2.png";
 
 const Description = () => {
   const navigate = useNavigate();
@@ -12,24 +13,28 @@ const Description = () => {
     <div className="screenHeight w-full bg-[#ffe9ff] border-[2px] ">
       <Header />
       <div className="relative w-full mainBox3 flex flex-col ">
+      <div className="w-full flex  items-center justify-center pb-8">
+              <img src={LOGO2} alt="logo" />
+            </div>
+
+        <div className="des_content overflow-scroll">
         <p>
-          I have opted to use the ONE IN A BILLION PERSONALITY REVEALER web link
-          of my own volition and agree to upload my picture and personal details
-          therein for participating in ONE IN A BILLION PERSONALITY REVEALER
-          activity.{" "}
+        An individual's, personality encompasses a collection of traits that constructs a particular set of behavior or ways of thinking which may resonate with their conduct in the society and work culture.
+
         </p>
         <p>
-          I state that the picture and personal details shared by me are true
-          and accurate and the company shall not have any liability arising from
-          its reliance on the same. I understand that no data will be retained
-          by the company post the event. I agree that the all contents used for
-          promotion of the event shall be the property of GE, and meant for its
-          use only.
+        This personality gamification is an interactive effort to reveal your unique professional personality, which could be from the following types : Confident, Versatile or Consistent; mirroring Omnipaque hallmarks. Omnipaque has been your trusted LOCM keeping radiology moving: Omnipaque’s established position as the WORKHORSE for BUSY RADIOLOGY departments, delivering Confident, Versatile, Consistent,  Quality, Reliability, Heritage, and Expertise are synonymous with Omnipaque. This has resulted in Omnipaque's success in delivering One billion+  doses of Omnipaque being used in over 1 billion procedures to date.
         </p>
 
-        <button onClick={handleClick} className="okBtn">
+       
+              </div>   
+
+              <button onClick={handleClick} className="okBtn">
           GO
         </button>
+       
+
+        
       </div>
     </div>
   );
